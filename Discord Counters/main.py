43f44b684a91b8ec3eb049fe18c0d5f0862c0ccb
tomGER <25822956+tumGER@users.noreply.github.com>
2019-04-15 15:58:21 +0200

    Counter: Improve performance by not checking that if statement every ms kek

--- Discord Counters/main.py
+++ Discord Counters/main.py
@@ -16,36 +16,33 @@ unmoderatedchannel = 1 #int of channel id for unmoderated
 
 @client.event
 async def on_ready():
-    xtime = time.time() - 300
     print('We have logged in as {0.user}'.format(client))
-
     while True:
-        if time.time() >= (xtime + 300):
-            c = 0
-            xtime = time.time()
-            for l in subreddit.mod.modqueue(only='submissions', limit=None):
-                c = c + 1 # Len doesn't work with ListingGenerators apperently so just fuck it and count like the good old days kek
-                print("Yes")
-            print("Editing Channel")
-            await client.get_channel(subchannel).edit(name=f"Mod-Queue: {str(c)}")
-            c = 0
-            print("Searching Modmail")
+        c = 0
+        for l in reddit.subreddit('deepfriedmemes').mod.modqueue(only='submissions', limit=None):
+            c = c + 1
+            print("Yes")
+        print("Editing Channel")
+        await client.get_channel(566707426972270628).edit(name=f"Mod-Queue: {str(c)}")
+        c = 0
+        print("Searching Modmail")
 
-            for m in subreddit.modmail.conversations(): # Has no limit
-                c = c + 1
-                print("Yes M")
-            print("Editing M Channel")
-            await client.get_channel(modmailchannel).edit(name=f"Mod-Mail: {str(c)}")
-            print("Finished")
+        for m in reddit.subreddit("deepfriedmemes").modmail.conversations():
+            c = c + 1
+            print("Yes M")
+        print("Editing M Channel")
+        await client.get_channel(566717121606844594).edit(name=f"Mod-Mail: {str(c)}")
+        print("Finished")
 
-            c = 0
-            print("Searching U")
+        c = 0
+        print("Searching U")
 
-            for m in subreddit.mod.unmoderated(limit=None):
-                c = c + 1
-                print("Yes U")
-            print("Editing U Channel")
-            await client.get_channel(unmoderatedchannel).edit(name=f"Unmoderated: {str(c)}")
-            print("Finished")
+        for m in reddit.subreddit("deepfriedmemes").mod.unmoderated(limit=None):
+            c = c + 1
+            print("Yes U")
+        print("Editing U Channel")
+        await client.get_channel(566723802822868993).edit(name=f"Unmoderated: {str(c)}")
+        print("Finished")
+        await asyncio.sleep(300)
 
 client.run(distoken)
