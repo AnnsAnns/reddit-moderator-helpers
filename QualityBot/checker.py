8c4625e8c44c4af2f8e19048100acf119cefa5c6
tomGER <tumger@outlook.de>
2019-09-23 19:18:44 +0200

    Update and add approval

--- QualityBot/checker.py
+++ QualityBot/checker.py
@@ -4,8 +4,15 @@ from config import *
 
 while True: # Loop
     for comment in reddit.redditor(bot_name).comments.new(limit=150):
-        if comment.subreddit.name == subreddit_name and comment.score <= -3: # Since the bot is used for multiple subs
-            comment.submission.report(f"Warning! Bot comment has reached a score of {comment.score}." \
-                "Please check the post!")
-            print("Detected Negative Karma!")
+        print(f"Found comment with {comment.score} karma!")
+        if comment.subreddit.name == subreddit_name:
+            if comment.score <= -3: # Since the bot is used for multiple subs
+                comment.submission.report(f"Warning! Bot comment has reached a score of {comment.score}." \
+                    "Please check the post!")
+                print("Detected Negative Karma!")
+        elif comment.score >= 100:
+            comment.edit("What a legendary fight! Thank you so much for letting us know that this post is good." \
+                         "\n\n --- \n\n I'm a bot created to make the moderation of this sub easier")
+            comment.submission.mod.approve()
+            print("Approved submission")
     time.sleep(900)
