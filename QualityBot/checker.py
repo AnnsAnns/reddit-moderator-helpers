b37392ab311c3c9c46e6ac166a05fdaf9835bf01
tomGER <tumger@outlook.de>
2019-09-23 20:07:57 +0200

    Reduce API Requests

--- QualityBot/checker.py
+++ QualityBot/checker.py
@@ -5,8 +5,8 @@ from config import *
 while True: # Loop
     for comment in reddit.redditor(bot_name).comments.new(limit=150):
         print(f"Found comment with {comment.score} karma!")
-        if comment.subreddit.name == subreddit_name:
-            if comment.score <= -3: # Since the bot is used for multiple subs
+        if comment.subreddit.name == subreddit_name and not comment.submission.hidden: #  Since the bot is used for multiple subs
+            if -2 > comment.score:
                 comment.submission.report(f"Warning! Bot comment has reached a score of {comment.score}." \
                     "Please check the post!")
                 print("Detected Negative Karma!")
@@ -15,4 +15,5 @@ while True: # Loop
                          "\n\n --- \n\n I'm a bot created to make the moderation of this sub easier")
             comment.submission.mod.approve()
             print("Approved submission")
+            comment.submission.hide()
     time.sleep(900)
